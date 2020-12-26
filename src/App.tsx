c330b3b0afe0f33629be15e260ed2c89c777a162
ukikagi <ukikagi@gmail.com>
2020-12-26 23:23:13 +0900

    Add description

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,37 @@ import { LEVEL_NROW, LEVEL_NCOL, parseLevel } from "./level";
 import { Direction } from "./point";
 import { sampleLevels } from "./sampleLevel";
 import { Box, Button, MenuItem, Select, TextField } from "@material-ui/core";
+import ReactMarkdown from "react-markdown";
 
 const defaultLevelString = sampleLevels[0].data;
+
+const description = `
+# Pink Virus Puzzle
+かつてパズルゲームサイト[ぼらＱ](https://web.archive.org/web/20060116224359/http://boraq.hp.infoseek.co.jp/main.htm)\
+に掲載されていたパズルゲーム Pink Virus のクローンです。
+
+## ルール
+鍵を全部集めて出口を目指しましょう。
+- 主人公は上下左右に移動することができます。
+  - 主人公は重力の影響を受けます：移動後、以下のいずれかの条件を満たすまで落下します。
+    1. 主人公のいるマスにハシゴかロープが存在する。
+    2. 主人公の下のマスに足場（レンガ・ブロック・ハシゴ・床）が存在する。
+  - レンガとブロックのあるマスは通過することができません。
+  - 上方向への移動はハシゴのあるマスでのみ可能です。
+- 主人公は自分の左下・右下にあるレンガを破壊することができます。
+  - 対象のレンガの上に他のレンガやブロックがある場合は破壊することができません。
+  - 同時に破壊することができるレンガは5個までです。6個めのレンガを破壊すると最初に破壊したレンガが復活します。
+  - 主人公が復活したレンガに埋まってしまった場合はゲームオーバーです。
+
+## 操作方法
+- ←/↑/→/↓: 移動
+- Z/X: 左下・右下のレンガを壊す
+- R: リスタート
+
+## 使用素材
+- "[Oubliette Tileset](https://safwyl.itch.io/oubliette-tileset)" by [safwyl](https://safwyl.itch.io/) (licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/))
+
+`;
 
 function App() {
   const [selectValue, setSelectValue] = useState(defaultLevelString);
@@ -65,6 +94,9 @@ function App() {
 
   return (
     <Box className="App">
+      <Box style={{ textAlign: "left" }}>
+        <ReactMarkdown children={description} />
+      </Box>
       <Board
         tabIndex={0}
         onKeyDown={onKeyDown}
